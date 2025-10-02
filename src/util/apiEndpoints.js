06f720789706c27e16caafcde22d5786cf592b4c
Tabrez Rabbani <169131435+tabrez-tech-09@users.noreply.github.com>
2025-10-02 22:14:55 +0530

    Update BASE_URL for deployment

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
 
 export const API_ENDPOINTS = {
